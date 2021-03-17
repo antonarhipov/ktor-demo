04fbaf2430d210b0d43728f57812ece09e98a198
Anton Arhipov <ant.arhipov@gmail.com>
2021-03-17 16:06:59 +0200

    test results for Async.kt

--- src/main/kotlin/com/example/Async.kt
+++ src/main/kotlin/com/example/Async.kt
@@ -59,3 +59,51 @@ private suspend fun ApplicationCall.respondHandlingLongCalculation(random: Rando
         }
     }
 }
+
+/**
+Testing:
+
+~$ ab -n 100 -c 100 http://localhost:8080/async
+This is ApacheBench, Version 2.3 <$Revision: 1826891 $>
+Copyright 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/
+Licensed to The Apache Software Foundation, http://www.apache.org/
+
+Benchmarking localhost (be patient).....done
+
+
+Server Software:        ktor-server-core/1.5.2
+Server Hostname:        localhost
+Server Port:            8080
+
+Document Path:          /async
+Document Length:        253 bytes
+
+Concurrency Level:      100
+Time taken for tests:   6.638 seconds
+Complete requests:      100
+Failed requests:        0
+Total transferred:      43900 bytes
+HTML transferred:       25300 bytes
+Requests per second:    15.07 [#/sec] (mean)
+Time per request:       6637.657 [ms] (mean)
+Time per request:       66.377 [ms] (mean, across all concurrent requests)
+Transfer rate:          6.46 [Kbytes/sec] received
+
+Connection Times (ms)
+min  mean[+/-sd] median   max
+Connect:        0   11   5.6     10      20
+Processing:  3280 3334  17.8   3339    3358
+Waiting:     3259 3333  18.6   3339    3358
+Total:       3280 3345  18.2   3351    3374
+
+Percentage of the requests served within a certain time (ms)
+50%   3351
+66%   3356
+75%   3358
+80%   3359
+90%   3362
+95%   3364
+98%   3369
+99%   3374
+100%   3374 (longest request)
+*/
